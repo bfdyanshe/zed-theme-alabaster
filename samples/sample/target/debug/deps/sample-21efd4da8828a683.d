1081f0712719acaab8985287610d2580b1438073
luozuojin <luozuojin@ft.tech>
2025-11-17 14:48:41 +0800

    feat: add functin, type, struct hightligh

---
+++ samples/sample/target/debug/deps/sample-21efd4da8828a683.d
@@ -0,0 +1,6 @@
+/Users/luozuojin/repo/zed-theme-alabaster/samples/sample/target/debug/deps/sample-21efd4da8828a683.d: src/main.rs src/sample.rs
+
+/Users/luozuojin/repo/zed-theme-alabaster/samples/sample/target/debug/deps/libsample-21efd4da8828a683.rmeta: src/main.rs src/sample.rs
+
+src/main.rs:
+src/sample.rs:
---
+++ samples/sample/target/debug/deps/sample-d165d7e5bc6af03e.d
@@ -0,0 +1,6 @@
+/Users/luozuojin/repo/zed-theme-alabaster/samples/sample/target/debug/deps/sample-d165d7e5bc6af03e.d: src/main.rs src/sample.rs
+
+/Users/luozuojin/repo/zed-theme-alabaster/samples/sample/target/debug/deps/libsample-d165d7e5bc6af03e.rmeta: src/main.rs src/sample.rs
+
+src/main.rs:
+src/sample.rs:
